2b1354d52961ce79463d9f16e1ae9b304d8045ca
FastInsOi <69037880+FastInsOi@users.noreply.github.com>
2022-07-01 21:27:46 +0300

    Setup for installation

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from dispag import __version__
+
+setup(
+    name='dispag',
+    version=__version__,
+
+    url='https://github.com/FastInsOi/DisPag',
+    author='FastInsOi',
+    author_email='fastinsoiisfaster@gmail.com',
+
+    py_modules=['dispag'],
+)
